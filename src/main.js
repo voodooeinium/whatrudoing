efdb380108df5ce868cbde1d4d36acf3a5ed37f3
xiallyvoodoo@gmail.com <xiallywoodoo@gmail.com>
2019-04-30 09:25:52 +0800

    修改为element-ui按需引入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import {
+  Form,
+  FormItem,
+  TimePicker,
+  Radio,
+  RadioGroup,
+  Select,
+  Option,
+  Cascader,
+  Button,
+  Loading
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
@@ -9,8 +20,16 @@ import 'babel-polyfill'
 import 'leaflet/dist/leaflet.css'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
-
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(TimePicker)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Cascader)
+Vue.use(Button)
+Vue.use(Loading)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
