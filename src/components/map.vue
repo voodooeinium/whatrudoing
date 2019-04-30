cd81eef46c9d7526eba8207b16c402efa4abff03
shuiRong <shuirong1997@icloud.com>
2019-04-30 22:18:44 +0800

    添加loader可以使用sass/scss;添加网页转换成图片的功能，供后续分享传播使用;

--- src/components/map.vue
+++ src/components/map.vue
@@ -50,7 +50,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .map-wrapper{
     height: 100%;
     width: 100%;
--- src/view/map.vue
+++ src/view/map.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="map-wrapper"
+    <div id='map-container'
     v-loading="showLoading"
     element-loading-text="加载中"
     element-loading-spinner="el-icon-loading"
@@ -58,11 +58,16 @@
             <p class="sum-count">此时共有<span>{{sum}}</span>人在加班</p>
             <p class="no-pay-count">其中<span>{{noPay}}</span>的人没有加班工资</p>
         </div>
+        <div class='image-cover' v-show='imageVisible'>
+            <img @click='closeImageModal' src='data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjZmZmZmZmIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0xOSA2LjQxTDE3LjU5IDUgMTIgMTAuNTkgNi40MSA1IDUgNi40MSAxMC41OSAxMiA1IDE3LjU5IDYuNDEgMTkgMTIgMTMuNDEgMTcuNTkgMTkgMTkgMTcuNTkgMTMuNDEgMTJ6Ii8+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+Cjwvc3ZnPgo=' alt='close' class='close-button'>
+        </div>
     </div>
 </template>
 
 <script>
 import lMap from '@/components/map';
+import html2canvas from "html2canvas";
+
 export default {
     components: {
         lMap
@@ -105,7 +110,8 @@ export default {
                 {value: 4, label: '私营企业'},
                 {value: 5, label: '中外合资企业'},
                 {value: 6, label: '外商投资企业'}
-            ]
+            ],
+            imageVisible: false // 是否展示预览图容器
         }
     },
     methods: {
@@ -128,13 +134,35 @@ export default {
                 self.isUnfold = false;
                 self.addMeTxt = '修福报';
             }, 0);
-        }
+        },
+        closeImageModal(){
+            this.imageVisible = false;
+            document.querySelector(".image-cover").removeChild(document.querySelector("canvas"));
+        },
+        /**
+         * @description 生成页面截图函数，将该DOM下的内容转换成canvas，并预览
+         */
+        generageScreenShot() {
+            let screenShot = document.querySelector("#map-container");
+            let width = screenShot.offsetWidth;
+            let height = screenShot.offsetHeight;
+            html2canvas(screenShot, {
+                allowTaint: true,
+                useCORS: true,
+                height: height,
+                width: width,
+                scale: 1 // 修改画质：1为正常，越小画质越低
+            }).then(canvas => {
+                document.querySelector(".image-cover").appendChild(canvas)
+                this.imageVisible = true;
+            });
+        },
     }
 }
 </script>
 
-<style>
-.map-wrapper{
+<style lang='scss'>
+#map-container{
     height: 100%;
     width:100%;
     overflow: hidden;
@@ -198,5 +226,35 @@ export default {
 .overtime-state-wrapper span{
     color: #e8e94f;
     padding: 0 5px;
+}
+
+.image-cover{
+    canvas{
+        position: fixed;
+        top: 0;
+        left: 0;
+        transform: scale(0.8);
+        opacity: 1;
+        z-index: 9999;
+        transition: transform 0.3s cubic-bezier(0.42, 0, 0.58, 1) 0s, opacity 0.3s cubic-bezier(0.42, 0, 0.58, 1) 0s, -webkit-transform 0.3s cubic-bezier(0.42, 0, 0.58, 1) 0s;
+    }
+}
+</style>
+
+<style scoped lang='scss'>
+.image-cover{
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.75);
+    img.close-button{
+        position: absolute;
+        right: 20px;
+        top: 20px;
+        cursor: pointer;
+    }
 }
 </style>
